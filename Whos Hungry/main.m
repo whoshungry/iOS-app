3ca5567fbc78fbc81de779b5b176f2e7a43f6b4d
Silmac <silveyra.92@gmail.com>
2014-11-05 21:20:07 -0600

    Initial Commit

---
+++ Whos Hungry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Whos Hungry
+//
+//  Created by administrator on 11/5/14.
+//  Copyright (c) 2014 WHK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
