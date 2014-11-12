516b0bb3952ee1eff467eaa74f5f3f1431f633c0
Silmac <silveyra.92@gmail.com>
2014-11-11 23:31:20 -0600

    add create_group and create_vote as well as HootLobby storing in NSUserDefaults

--- Whos Hungry/SummaryViewController.h
+++ Whos Hungry/SummaryViewController.h
@@ -11,11 +11,16 @@
 #import <Parse/Parse.h>
 #import <ParseFacebookUtils/PFFacebookUtils.h>
 #import <MapKit/MapKit.h>
+#import "HootLobby.h"
 #import <CoreLocation/CoreLocation.h>
+#import "AFNetworking.h"
+#import "AFHTTPRequestOperation.h"
 
 @interface SummaryViewController : UIViewController<CLLocationManagerDelegate, MKMapViewDelegate> {
     CLLocationManager *locationManager;
 }
+
+@property (strong, nonatomic) HootLobby* currentLobby;
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 - (IBAction)goHome:(id)sender;
